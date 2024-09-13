0bcbe9489430bc87c10381d7000cb788c7921b12
LongYinan <lynweklm@gmail.com>
2024-09-14 00:19:10 +0800

    error handling right on unix

--- src/lib.rs
+++ src/lib.rs
@@ -122,8 +122,43 @@ pub fn nice(incr: i32) -> Result<i32> {
 pub fn get_current_process_priority() -> Result<i32> {
   #[cfg(unix)]
   unsafe {
+    extern "C" {
+      #[cfg(not(any(target_os = "dragonfly", target_os = "vxworks")))]
+      #[cfg_attr(
+        any(
+          target_os = "linux",
+          target_os = "emscripten",
+          target_os = "fuchsia",
+          target_os = "l4re",
+          target_os = "hurd",
+        ),
+        link_name = "__errno_location"
+      )]
+      #[cfg_attr(
+        any(
+          target_os = "netbsd",
+          target_os = "openbsd",
+          target_os = "android",
+          target_os = "redox",
+          target_env = "newlib"
+        ),
+        link_name = "__errno"
+      )]
+      #[cfg_attr(
+        any(target_os = "solaris", target_os = "illumos"),
+        link_name = "___errno"
+      )]
+      #[cfg_attr(target_os = "nto", link_name = "__get_errno_ptr")]
+      #[cfg_attr(
+        any(target_os = "freebsd", target_vendor = "apple"),
+        link_name = "__error"
+      )]
+      #[cfg_attr(target_os = "haiku", link_name = "_errnop")]
+      #[cfg_attr(target_os = "aix", link_name = "_Errno")]
+      fn errno_location() -> *mut core::ffi::c_int;
+    }
     // clear the last error
-    *libc::__errno_location() = 0;
+    *errno_location() = 0;
     let ret = libc::getpriority(libc::PRIO_PROCESS, 0);
     // recheck the os error
     let os_error = std::io::Error::last_os_error();
