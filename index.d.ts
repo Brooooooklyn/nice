6d10bfd724a6843c690d7bdb337e576f5e1e5132
LongYinan <lynweklm@gmail.com>
2025-07-03 17:22:11 +0800

    fix: don't use const enum

--- index.d.ts
+++ index.d.ts
@@ -32,7 +32,7 @@ export declare function getCurrentProcessPriority(): number
  */
 export declare function nice(incr?: number | undefined | null): number
 
-export declare const enum WindowsThreadPriority {
+export declare enum WindowsThreadPriority {
   ThreadModeBackgroundBegin = 65536,
   ThreadModeBackgroundEnd = 131072,
   ThreadPriorityAboveNormal = 1,
