67344311f09d9b179c1a2f29471396d2d90f27fb
LongYinan <lynweklm@gmail.com>
2025-07-04 17:45:29 +0800

    test: add WindowsThreadPriority case

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import { nice, getCurrentProcessPriority } from '../index.js'
+import { nice, getCurrentProcessPriority, WindowsThreadPriority } from '../index.js'
 
 test('should be able to call nice', (t) => {
   t.notThrows(() => {
@@ -11,4 +11,16 @@ test('should be able to call nice', (t) => {
 
 test('should be able to get current process priority', (t) => {
   t.is(typeof getCurrentProcessPriority(), 'number')
+})
+
+test('should be able to get WindowsThreadPriority', (t) => {
+  t.is(WindowsThreadPriority.ThreadModeBackgroundBegin, 65536)
+  t.is(WindowsThreadPriority.ThreadModeBackgroundEnd, 131072)
+  t.is(WindowsThreadPriority.ThreadPriorityAboveNormal, 1)
+  t.is(WindowsThreadPriority.ThreadPriorityBelowNormal, -1)
+  t.is(WindowsThreadPriority.ThreadPriorityHighest, 2)
+  t.is(WindowsThreadPriority.ThreadPriorityIdle, -15)
+  t.is(WindowsThreadPriority.ThreadPriorityLowest, -2)
+  t.is(WindowsThreadPriority.ThreadPriorityNormal, 0)
+  t.is(WindowsThreadPriority.ThreadPriorityTimeCritical, 15)
 })
